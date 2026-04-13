ad8e4a1e82bc361239a7e8e6c09e6d7e0105674b
Nadja Brix Koch <n.koch@samsung.com>
2026-04-13 18:11:09 +0200

    task 3a
    Signed-off-by: Nadja Brix Koch <n.koch@samsung.com>

---
+++ CoDepend/Infra/Repository.cs
@@ -0,0 +1,16 @@
+using CoDepend.Application;
+using CoDepend.Domain.Models;
+
+namespace CoDepend.Infra;
+
+public class Repository : IRepository
+{
+    public ProjectDependencyGraph? GetSnapshot()
+    {
+        return null;
+    }
+
+    public void SetSnapshot(ProjectDependencyGraph snapshot)
+    {
+    }
+}
