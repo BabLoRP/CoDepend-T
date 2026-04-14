98f330b103bd3015c6eb88845e7301152811f2eb
rakulmaria <rakulmaria@hotmail.com>
2026-04-14 11:08:48 +0200

    3a: repository setup

---
+++ CoDepend/Infra/Repository.cs
@@ -0,0 +1,14 @@
+using CoDepend.Domain.Models;
+
+namespace CoDepend.Infra;
+
+public class Repository
+{
+    public ProjectDependencyGraph? GetSnapshot() {
+        return null;
+    }
+
+    public void SetSnapshot(ProjectDependencyGraph snapshot) {
+
+    }
+}
