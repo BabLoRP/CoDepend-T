2b4d17a7b54587890b159ef97b7c69abaeaf758b
Sebastian Fugmann <sf15006@gmail.com>
2026-04-15 18:41:22 +0200

    Initial start of 2

---
+++ CoDepend/Application/ExclusionManager.cs
@@ -0,0 +1,24 @@
+namespace CoDepend.Application;
+
+public sealed class ExclusionManager
+{
+    private sealed record ExclusionRule
+    {
+        
+    }
+
+    public static bool IsExcluded()
+    {
+        return true;
+    }
+
+    public static void NormalizeExclusionEntry()
+    {
+        
+    }
+
+    public static void CompileExclusions()
+    {
+        
+    }
+}
