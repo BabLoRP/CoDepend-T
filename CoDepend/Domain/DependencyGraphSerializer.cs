f84cb13b9f2d155ef0606d2dab110fae933086a4
Sebastian Fugmann <sf15006@gmail.com>
2026-04-15 18:01:14 +0200

    Removed infra reference

--- CoDepend/Domain/DependencyGraphSerializer.cs
+++ CoDepend/Domain/DependencyGraphSerializer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using CoDepend.Domain.Models;
 using CoDepend.Domain.Models.Records;
-using CoDepend.Infra;
 using MessagePack;
 using MessagePack.Resolvers;
 
